b459baa457c81cb9ca86ad91d2465a41d8d3f5e9
VladaHejda <hejda@vspdata.cz>
2015-04-24 10:45:30 +0200

    DI container initialize method: Tracy Bar dependency problem

---
+++ app/model/Panel.php
@@ -0,0 +1,18 @@
+<?php
+
+class Panel implements \Tracy\IBarPanel
+{
+
+	public function __construct(MyService $myService)
+	{
+	}
+
+	public function getTab()
+	{
+	}
+
+	public function getPanel()
+	{
+	}
+
+}
